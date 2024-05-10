7738672a26d565cbb0bc87c43c9dc01938281b85
Dhafin Salman <dhafinsalman31@gmail.com>
2024-05-10 09:21:21 +0700

    commit

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,10 @@
 let firebaseConfig = {
-    // Enter your firebase credentials
+    apiKey: "AIzaSyCO2a8ddpkdnNToyetjblPQtM6_gyzw104",
+    authDomain: "film-blogging.firebaseapp.com",
+    projectId: "film-blogging",
+    storageBucket: "film-blogging.appspot.com",
+    messagingSenderId: "650126788053",
+    appId: "1:650126788053:web:30eebd93ad54dd998c695c"
 };
 
 firebase.initializeApp(firebaseConfig);

01747c857e1daf8caf0c8644f999fd8f269da722
Dhafin Salman <dhafinsalman31@gmail.com>
2024-05-10 09:28:50 +0700

    commit

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.get('/editor', (req, res) => {
 app.post('/upload', (req, res) => {
     let file = req.files.image;
     let date = new Date();
+
     // image name
     let imagename = date.getDate() + date.getTime() + file.name;
     // image upload path
@@ -27,13 +28,21 @@ app.post('/upload', (req, res) => {
     // create upload
     file.mv(path, (err, result) => {
         if(err){
-                throw err;
-            } else{
-                // our image upload path
-                } res.json('uploads/${imagename}')
+            throw err;
+        } else{
+            // our image upload path
+            res.json(`uploads/${imagename}`)
+        }
     })
 })
 
+app.get("/:blog", (req, res) => {
+    res.sendFile(path.join(initial_path, "blog.html"));
+})
+
+app.use((req, res) => {
+    res.json("404");
+})
 
 app.listen("3000", () => {
     console.log('listening......');
